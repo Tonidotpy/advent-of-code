2de51c747a0c4ab01189cb28f427ee24c88c2a98
Antonio Gelain <antonio.gelain2@gmail.com>
2022-12-12 14:35:26 +0100

    Solved twelfth AoC 2022 problem

---
+++ 2022/12-12/first/first.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline int at(int r, int c, int n) {
+    return r * n + c;
+}
+inline pair<int, int> to(int i, int n) {
+    return {i / n, i % n};
+}
+
+int main() {
+    freopen("../input.txt", "r", stdin);
+    
+    vector<vector<int> > g(1);
+    pair<int, int> begin, end;
+
+    string s;
+    int row = 0, col = 0;
+    while(cin >> s) {
+        for (col=0; col < s.size(); col++) {
+            if (s[col] == 'S') {
+                g[row].push_back(0);
+                begin = {row, col};
+            }
+            else if (s[col] == 'E') {
+                g[row].push_back('z' - 'a');
+                end = {row, col};
+            }
+            else
+                g[row].push_back(s[col] - 'a');
+        }
+
+        g.push_back(vector<int>());
+        row++;
+    }
+
+    int b = at(begin.first, begin.second, col);
+    int e = at(end.first, end.second, col);
+
+    vector<bool> visited (row * col);
+    queue<pair<int, int> > q;
+    q.push({ b, 0 });
+    while (!q.empty()) {
+        auto aux = q.front();
+        q.pop();
+
+        if (aux.first == e) {
+            cout << aux.second << "\n";
+            return 0;
+        }
+
+        if (visited[aux.first])
+            continue;
+
+        visited[aux.first] = true;
+
+        auto a = to(aux.first, col);
+        // Top
+        if (a.first - 1 >= 0 &&
+            g[a.first - 1][a.second] <= g[a.first][a.second] + 1)
+            q.push({ at(a.first - 1, a.second, col), aux.second + 1 });
+        // Bottom
+        if (a.first + 1 < row &&
+            g[a.first + 1][a.second] <= g[a.first][a.second] + 1)
+            q.push({ at(a.first + 1, a.second, col), aux.second + 1 });
+        // Left
+        if (a.second - 1 >= 0 &&
+            g[a.first][a.second - 1] <= g[a.first][a.second] + 1)
+            q.push({ at(a.first, a.second - 1, col), aux.second + 1 });
+        // Right
+        if (a.second + 1 < col &&
+            g[a.first][a.second + 1] <= g[a.first][a.second] + 1)
+            q.push({ at(a.first, a.second + 1, col), aux.second + 1 });
+    }
+}
